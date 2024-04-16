eee5e28fb3439561c1eacfc7525cabc0f5423ef2
Haejeong079 <127839004+Haejeong079@users.noreply.github.com>
2024-04-16 20:01:49 +0900

    first commit

---
+++ src/main/java/inhatc/cse/spring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.Spring.controller;
+
+import org.example.Spring.dto.PersonDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public PersonDto hello(){
+        PersonDto person = new PersonDto("홍길동", 20, 172.4);
+
+        return person;
+    }
+}
