9b5d73d221922ecc54c7f8fd9d23cdf331593dad
Haejeong079 <127839004+Haejeong079@users.noreply.github.com>
2024-05-07 23:41:08 +0900

    spring project HelloController, jsp 링크반 5/7

--- src/main/java/inhatc/cse/spring/repository/MemberRepository.java
+++ src/main/java/inhatc/cse/spring/repository/MemberRepository.java
@@ -1,14 +1,20 @@
 package inhatc.cse.spring.repository;
 
 import inhatc.cse.spring.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+
+    private final SqlSessionTemplate sqlSession;
 
 
     public int save(MemberDto memberDto) {
         System.out.println("==========" + memberDto);
-        return 0;
+        int resert = sqlSession.insert("Member.save", memberDto);
+        return resert;
     }
 }
